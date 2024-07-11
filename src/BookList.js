b33c107bdfb9311e97d221c0e4776e8a55427f2e
nikhilsontakke <nikhilsontakke111@gmail.com>
2024-07-11 20:49:43 +0200

    Add shelf and shelf number to table

--- src/BookList.js
+++ src/BookList.js
@@ -10,35 +10,37 @@ const BookList = ({ books }) => {
     <div className="book-list">
       <table>
         <thead>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>ISBN</th>
-            <th>Publisher</th>
-            <th>No of Pages</th>
-            <th>Language</th>
-            <th>Format</th>
-            <th>Genre</th>
-            <th>Edition</th>
-            <th>Publishing Year</th>
-          </tr>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>ISBN</th>
+          <th>Publisher</th>
+          <th>No of Pages</th>
+          <th>Language</th>
+          <th>Format</th>
+          <th>Genre</th>
+          <th>Edition</th>
+          <th>Publishing Year</th>
+          <th>Shelf-ID</th>
+          <th>Shelf-Row</th>
+        </tr>
         </thead>
         <tbody>
           {books.map((book) => (
-            <tr key={book.id}>
-              <td>{book.id}</td>
-              <td>{book.title}</td>
-              <td>{book.author}</td>
-              <td>{book.isbn}</td>
-              <td>{book.publisher}</td>
-              <td>{book.noOfPages}</td>
-              <td>{book.language}</td>
-              <td>{book.format}</td>
-              <td>{book.genre}</td>
-              <td>{book.edition}</td>
-              <td>{book.publishingYear}</td>
-            </tr>
+              <tr key={book.id}>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>{book.isbn}</td>
+                <td>{book.publisher}</td>
+                <td>{book.noOfPages}</td>
+                <td>{book.language}</td>
+                <td>{book.format}</td>
+                <td>{book.genre}</td>
+                <td>{book.edition}</td>
+                <td>{book.publishingYear}</td>
+                <td>{book.shelf}</td>
+                <td>{book.shelfrow}</td>
+              </tr>
           ))}
         </tbody>
       </table>
